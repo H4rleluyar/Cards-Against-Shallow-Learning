dad19ad4ff9aab470a91af90f52b5fceca53b15c
Gabrielle Kim <gabrielle.kim@sjsu.edu>
2021-10-19 22:09:20 -0700

    Created Deck class and implemented methods

--- src/Deck.java
+++ src/Deck.java
@@ -1,2 +1,40 @@
-public class Deck {
+import java.util.ArrayList;
+
+public class Deck extends Card{
+	private String color; 					// color of deck; white (center deck)  or black (questions) or green (player's hand)  
+	private int numOfCards; 				// number of cards in a deck
+	private ArrayList<Card> currentDeck; 	// stores a deck as an arraylist
+	
+	// constructor for Deck 
+	public Deck(ArrayList<Card> currentDeck)
+	{
+		this.currentDeck = currentDeck; 
+		
+	}
+	
+	// getter and setter for deck color (either white or black) 
+	public String getColor() { return color; }
+	public void setColor(String newColor) { this.color = newColor; }
+	
+	
+	// setter and getter for number of cards in a deck
+	public int getNumOfCards() { return numOfCards; }
+	public void setNumOfCards(int numOfCards) { this.numOfCards = numOfCards;} 
+	
+	// adds a card to the current Deck 
+	public void addCard(Card c) 
+	{
+		this.currentDeck.add(c);
+	}
+	
+	// removes a card from the current Deck 
+	public void removeCard(int index)
+	{
+		this.currentDeck.remove(index);
+	}
+	
+	
+	
+	
+	
 }
