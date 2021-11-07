10ff8b983d157fbaec9881349120cf5593bddedb
Henry Lai <henrylai@Henrys-MacBook-Pro.local>
2021-11-07 01:14:25 -0700

    Implemeted load, whiteToString, blackToString methods (HL404-41, HL404-42, HL404-44)

--- src/ConsoleTable.java
+++ src/ConsoleTable.java
@@ -1,10 +1,57 @@
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.io.File;
+import java.util.Scanner;
 
 public class ConsoleTable {
     Player[] playerArr = new Player[4];
-    ArrayList<Card> whiteDeck = new ArrayList<>();
-    ArrayList<Card> blackDeck = new ArrayList<>();
-    ArrayList<Card>[] handDeck = new ArrayList[4];
-    ArrayList<Object> chosenCard = new ArrayList<>();
+    Deck whiteDeck = new Deck();
+    Deck blackDeck = new Deck();
+    ArrayList<Object> chosenCard = new ArrayList<Object>();
 
+    boolean load(String fileDir){
+        File file = new File(fileDir);
+        try {
+            Scanner scan = new Scanner(file);
+            String line;
+            Card c;
+            while(scan.hasNextLine()){
+                line = scan.nextLine();
+                //ignores empty and commented lines
+                if(!line.isEmpty()){
+                    if(line.charAt(0) != '#') {
+                        //black card
+                        if(line.charAt(0) == 'B'){
+                            blackDeck.addCard(new Card(line.substring(2)));
+//                            System.out.println(line.substring(2));
+                        }
+                        //white card
+                        else if(line.charAt(0) == 'W'){
+                            whiteDeck.addCard(new Card(line.substring(2)));
+//                            System.out.println(line.substring(2));
+                        }
+                    }
+                }
+            }
+        }
+        catch (FileNotFoundException e) {
+            //file not found
+            return false;
+        }
+        return true;
+    }
+
+    String whiteToString(){
+        String retStr = "";
+        retStr += "White Deck Cards:\n";
+        retStr += whiteDeck.toString();
+        return retStr;
+    }
+
+    String blackToString(){
+        String retStr = "";
+        retStr += "Black Deck Cards:\n";
+        retStr += blackDeck.toString();
+        return retStr;
+    }
 }
