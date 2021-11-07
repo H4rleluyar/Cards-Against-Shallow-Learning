6f0aebd5b9390932e0f5515fa762d5c1fe4ff2cd
Henry Lai <henrylai@Henrys-MacBook-Pro.local>
2021-11-07 01:33:48 -0700

    moved txt file to project folder, and updated fileDir for easier testings

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String args[]){
         ConsoleTable ct = new ConsoleTable();
-        ct.load("/Users/henrylai/Desktop/test.txt");
+        ct.load("test.txt");
         System.out.println(ct.whiteToString());
         System.out.println(ct.blackToString());
     }
