fb92629c255085ad4fec88616c3f97fc9b36b462
Henry Lai <henrylai@Henrys-MacBook-Pro.local>
2021-11-06 17:52:21 -0700

    Created variable definitions

--- src/ConsoleTable.java
+++ src/ConsoleTable.java
@@ -1,5 +1,10 @@
 import java.util.ArrayList;
 
 public class ConsoleTable {
+    Player[] playerArr = new Player[4];
+    ArrayList<Card> whiteDeck = new ArrayList<>();
+    ArrayList<Card> blackDeck = new ArrayList<>();
+    ArrayList<Card>[] handDeck = new ArrayList[4];
+    ArrayList<Object> chosenCard = new ArrayList<>();
 
 }
