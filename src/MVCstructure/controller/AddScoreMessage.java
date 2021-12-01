b299571a509e30711a865d9370fc7e4430860dee
Henry <henryfantlcode@gmail.com>
2021-12-01 14:24:03 -0800

    comment at the top of addscoremessage.java

--- src/MVCstructure/controller/AddScoreMessage.java
+++ src/MVCstructure/controller/AddScoreMessage.java
@@ -1,5 +1,12 @@
 package MVCstructure.controller;
 
+/*
+From Lai 12/1/21 @ 1:40pm
+Try to implement new add score buttons
+Make it so that the number of buttons corresponds to the number player in the game
+You can do that by creating 4 (max player we can have for now) individual buttons
+And make them visible or not according to the number of players After the start game button pressed"
+ */
 public class AddScoreMessage implements Message {
 
     private int score = 0; //all players start with a score of zero
