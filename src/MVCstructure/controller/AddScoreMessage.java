8cc04819d634af10891fb6cd16015fa9da82ebcf
Henry <henryfantlcode@gmail.com>
2021-12-01 14:21:16 -0800

    testing a comment in addscoremessage.java

--- src/MVCstructure/controller/AddScoreMessage.java
+++ src/MVCstructure/controller/AddScoreMessage.java
@@ -3,6 +3,7 @@ package MVCstructure.controller;
 public class AddScoreMessage implements Message {
 
     private int score = 0; //all players start with a score of zero
+    //testing a change from Henry Fan
 
     public AddScoreMessage(int score){
         this.score = score;
